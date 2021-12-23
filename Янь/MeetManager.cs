871fcd8dfa1892627adcfa9a33cbea0ab8615544
Diana001a <di89101630730@mail.ru>
2021-12-23 19:22:39 +0300

    Initial commit

---
+++ Янь/MeetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Янь.Models;
+
+namespace Янь
+{
+    public class MeetManager
+    {
+        List<Meet> meets = new List<Meet>();
+        public void AddNewMeet(string name, DateTime meetTime)
+        {
+            Meet newMeet = new Meet() { Name = name, CreateTime = DateTime.Now };
+            meets.Add(newMeet);
+        }
+    }
+}
